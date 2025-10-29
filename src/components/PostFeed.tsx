7fd549358f61bd335d2ae5c028bd8135ebe0f5e2
AliArif5781 <aliairf92@gmail.com>
2025-10-29 09:40:54 +0500

    PostFeed.tsx

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -84,7 +84,7 @@ const PostFeed = () => {
     const diffInMinutes = Math.floor(diffInSeconds / 60);
     const diffInHours = Math.floor(diffInMinutes / 60);
     const diffInDays = Math.floor(diffInHours / 24);
-    const diffInWeeks = Math.floor(diffInDays / 7);
+    // const diffInWeeks = Math.floor(diffInDays / 7);
 
     if (diffInSeconds < 60) {
       return "Just now";
