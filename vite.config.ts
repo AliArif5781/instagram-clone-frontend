72db0a36a49848f167e94b1c3cd0c99f4f4d2cf7
AliArif5781 <aliairf92@gmail.com>
2025-10-29 10:12:49 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import analysis from "vite-bundle-analyzer"; // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), analysis()],
 });
